e27ef00cc6a1478c826261f1b2cfb745cbbf0376
= <=>
2024-07-25 00:32:58 +0300

    first commit

---
+++ init.lua
@@ -0,0 +1,2 @@
+require("conf")
+print("LOADED: init.lua")
---
+++ lua/conf/init.lua
@@ -0,0 +1,6 @@
+require("conf.remap")
+vim.opt.termguicolors = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4,
+print("LOADED: /lua/conf/init.lua")
